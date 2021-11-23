cf8862732c9f07e6d3698ce77aac14f77e5efff1
Krzysztof Bogucki <krzysztof.bogucki@asseco.pl>
2021-11-23 09:25:25 +0100

    init

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyC47TzjE3zc6HZjarXBJ7Bw5_pAS-pQvXI",
+    authDomain: "iot-storage-db5ff.firebaseapp.com",
+    databaseURL: "https://iot-storage-db5ff-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "iot-storage-db5ff",
+    storageBucket: "iot-storage-db5ff.appspot.com",
+    messagingSenderId: "962548898934",
+    appId: "1:962548898934:web:db4ab1d4e8d0b18882d357"
+  }
 };
