eeed1a47c52c7899c2d0e8a7d5d44015e527108c
Krzysztof Bogucki <krzysztof.bogucki@asseco.pl>
2021-11-24 16:42:30 +0100

    clear logs

--- src/app/@views/home/home.component.ts
+++ src/app/@views/home/home.component.ts
@@ -30,16 +30,15 @@ export class HomeComponent implements OnInit {
   ngOnInit(): void {
     this.dbService.getCurrent().valueChanges().subscribe((data) => {
       this.data = data;
-      console.log("TU",this.data);
       if(this.data.length == 0)
         return;
 
       this.datetime = this.utils.epochStringToDateTime(this.data[0].epochTime);
 
-      this._tempCoOut = this.utils.stringToFloat(this.tempCoOut?.replace("℃",""));
-      this._tempCoIn= this.utils.stringToFloat(this.tempCoIn?.replace("℃",""));
-      this._tempCwuOut= this.utils.stringToFloat(this.tempCwuOut?.replace("℃",""));
-      this._tempOut= this.utils.stringToFloat(this.tempOut?.replace("℃",""));
+      this._tempCoOut = this.utils.stringToFloat(this.tempCoOut);
+      this._tempCoIn= this.utils.stringToFloat(this.tempCoIn);
+      this._tempCwuOut= this.utils.stringToFloat(this.tempCwuOut);
+      this._tempOut= this.utils.stringToFloat(this.tempOut);
 
       // @ts-ignore
       this.tempCoOut = this.utils.stringToFloat(this.data[0]?.value[3].tempC);
