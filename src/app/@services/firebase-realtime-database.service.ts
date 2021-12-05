2187e64a8eb6f09893a6c90f591212107bc196c2
cris <krzysztof.lubien@icloud.com>
2021-12-05 13:32:38 +0100

    some change

--- src/app/@services/firebase-realtime-database.service.ts
+++ src/app/@services/firebase-realtime-database.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
 import {AngularFireDatabase, AngularFireList} from "@angular/fire/compat/database";
 import {Observable} from "rxjs";
 import {SensorModel} from "../@models/SensorModel";
